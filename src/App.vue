7da98565bb92b60027cb2ad4f22cb7ec47d46a0d
= <kelraf11746@gmail.com>
2019-11-12 12:39:41 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-link class="btn" to="/">Home</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+ 
+}
+</script>
+
+<style>
+
+</style>
